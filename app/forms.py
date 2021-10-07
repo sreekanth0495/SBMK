60b27ae281c50772192e6bab7ea230da3abf6c99
sreekanth0495 <mcsreekanth0495@gmail.com>
2021-10-07 21:52:59 +0530

    added food application

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,16 @@ from django import forms
 from .models import *
 from durationwidget.widgets import TimeDurationWidget
 
+
 class RecipeForm(forms.ModelForm):
-    prep = forms.DurationField(widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False), required=False)
-    cook = forms.DurationField(widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False), required=False)
-    
+    prep = forms.DurationField(
+        widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False),
+        required=False)
+    cook = forms.DurationField(
+        widget=TimeDurationWidget(show_days=False, show_hours=True, show_minutes=True, show_seconds=False),
+        required=False)
+
     class Meta:
         model = Recipe
         fields = '__all__'
         exclude = ('author',)
-        
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, AuthenticationForm
 
+
 def signup(request):
     context = {}
     if request.method == 'POST':
